03382ba7cb346e4db1b2c6645c5700e675503fc3
Monnapse <masonshuber@gmail.com>
2023-09-23 10:21:07 -0600

    fix

--- tests/example.py
+++ tests/example.py
@@ -1,10 +1,11 @@
 # pip install . or set PYTHONPATH=%PYTHONPATH%;D:\Documents\Packages\Python\Gitor\src
 
-#import os
-#from gitor import Git, api
-#
-#MyGithubConnection = Git("Monnapse",os.environ.get("GITHUB_TOKEN"))
-#MyPrivateRepo = MyGithubConnection.get_sub_repository("monnapse-website", "app.py", True)
-#MyPublicRepo = MyGithubConnection.get_repository("flow")
+import os
+from gitor import Git, api
 
-#print(MyPublicRepo)
+#
+MyGithubConnection = Git("Monnapse",os.environ.get("GITHUB_TOKEN"))
+MyPrivateRepo = MyGithubConnection.get_sub_repository("monnapse-website", "app.py", True)
+MyPublicRepo = MyGithubConnection.get_repository("flow")
+
+print(MyPublicRepo)
