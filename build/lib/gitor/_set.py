296c50cc5cb793aaf6de709e0228b2538170443f
Monnapse <masonshuber@gmail.com>
2023-09-23 09:25:44 -0600

    fix

--- build/lib/gitor/_set.py
+++
@@ -1,9 +0,0 @@
-def find_value_in_json(JSON: set, attribute: str, value: str):
-    """
-    Find the set that has attribute that is equal to value
-    """
-    for i in JSON:
-        if i[attribute] == value:
-            return i
-        
-    print(f"Error: could not find {attribute} - {value}")
--- tests/example.py
+++ tests/example.py
@@ -1,10 +1,10 @@
 # pip install . or set PYTHONPATH=%PYTHONPATH%;D:\Documents\Packages\Python\Gitor\src
 
-import os
-from gitor import Git, api
+#import os
+#from gitor import Git, api
+#
+#MyGithubConnection = Git("Monnapse",os.environ.get("GITHUB_TOKEN"))
+#MyPrivateRepo = MyGithubConnection.get_sub_repository("monnapse-website", "app.py", True)
+#MyPublicRepo = MyGithubConnection.get_repository("flow")
 
-MyGithubConnection = Git("Monnapse",os.environ.get("GITHUB_TOKEN"))
-MyPrivateRepo = MyGithubConnection.get_sub_repository("monnapse-website", "app.py", True)
-MyPublicRepo = MyGithubConnection.get_repository("flow")
-
-print(MyPublicRepo)
+#print(MyPublicRepo)
