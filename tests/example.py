be57f919af5f505372644a6a09ba7a79afb208b9
Monnapse <masonshuber@gmail.com>
2023-09-22 23:53:46 -0600

    update

--- tests/example.py
+++ tests/example.py
@@ -1,4 +1,4 @@
-# set PYTHONPATH=%PYTHONPATH%;D:\Documents\Packages\Python\Gitor\src
+# pip install . or set PYTHONPATH=%PYTHONPATH%;D:\Documents\Packages\Python\Gitor\src
 
 import os
 from gitor import Git, api
@@ -7,4 +7,4 @@ MyGithubConnection = Git("Monnapse",os.environ.get("GITHUB_TOKEN"))
 MyPrivateRepo = MyGithubConnection.get_sub_repository("monnapse-website", "app.py", True)
 MyPublicRepo = MyGithubConnection.get_repository("flow")
 
-print(MyPrivateRepo,MyPublicRepo)
+print(MyPublicRepo)
